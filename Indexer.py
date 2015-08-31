eac6e883f3123b50cb0a5f027693ff29424e0752
Ilija Vukotic <ivukotic@cern.ch>
2015-08-31 16:33:40 -0500

    4th

--- Indexer.py
+++ Indexer.py
@@ -8,7 +8,8 @@ with open('dsnames.txt', 'r') as f:
 
 dss=read_data[2:]
 
-import os
+import os, Queue
+from threading import Thread
 import rucio
 import rucio.client
 import rucio.common.config as conf
@@ -20,19 +21,50 @@ else:
     print "no RUCIO_ACCOUNT environment found. Please set it before using this program."
     sys.exit(1) 
 
+class DataSet:
+    def __init__(self,line):
+        self.creator="unknown"
+        self.datatype="unknown"
+        spl=line.split(':')
+        self.scope=spl[0]
+        self.name=spl[1]    
+        self.size=0
+        self.files=0
+        
+        w=self.scope.split('.')
+        if w[0].startswith('user') or w[0].startswith('group'):
+            self.creator=w[1]
+            
+        if self.name.count(".AOD.")>0: self.datatype="AOD"
+        elif self.name.count(".ESD.")>0: self.datatype="ESD"
+        elif self.name.count(".RAW")>0: self.datatype="RAW"
+        elif self.name.count(".RDO")>0: self.datatype="RDO"
+        wo=self.name.split(".")
+        for w in wo:
+            if w.startswith("DESD_") or w.startswith("DAOD_"): 
+                self.datatype=w;
+                break 
+                
+    def getFilesSize(self):
+        cont=rucio.client.didclient.DIDClient().get_did(scope,DS)
+        self.files = cont['length']
+        self.size  = cont['bytes']      
+        
+    def prnt(self):
+        print self.scope,self.name, self.size, self.files, self.creator, self.datatype  
+
 rrc=rucio.client.replicaclient.ReplicaClient()
 
 
-def decodeDT(ds):
-    wo=ds.split(".")
-    for w in wo:
-        if w.startswith("DESD_") or w.startswith("DAOD_"): return w; 
-    return "unknown"
-
-def getFilesSize(scope, DS):
-    cont=rucio.client.didclient.DIDClient().get_did(scope,DS)
-    return cont['length'],cont['bytes']
     
+def worker():
+    while True:
+        f=q.get()
+        f.getFilesSize()
+        f.prnt()
+        print "still in queue: ", q.qsize()
+        q.task_done()
+
 
 es = Elasticsearch("uct2-es-head:9200")
 
@@ -42,44 +74,40 @@ es.indices.delete(index=IndName, ignore=[400, 404])
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
 es.indices.create(index=IndName, ignore=400)
 
-actions = []
 
+# created threads
+q=Queue.Queue()
+for i in range(int(opts.Streams)):
+     t = Thread(target=worker)
+     t.daemon = True
+     t.start()
+     
+DataSets=[]
 for DS in dss:
     DS=DS.strip()
-    scope=""
-    creator="unknown"
-    datatype="unknown"
-    
-    spl=DS.split(':')
-    scope=spl[0]
-    DS=spl[1]
-    
-    filesSizes=getFilesSize(scope,DS)
-    
-    w=scope.split('.')
-    if w[0].startswith('user') or w[0].startswith('group'):
-        creator=w[1]
-    
-    if DS.count(".AOD.")>0: datatype="AOD"
-    elif DS.count(".ESD.")>0: datatype="ESD"
-    elif DS.count(".RAW")>0: datatype="RAW"
-    elif DS.count(".RDO")>0: datatype="RDO"
-    else: datatype=decodeDT(DS)
-    print scope, creator, datatype, DS
+    nds=DataSet(DS)
+    DataSets.append(DataSet(DS))
+    q.put(nds)
+
+q.join()     
+
+print "indexing ..."
+actions = []
+for DS in dss:
     action = {
         "_index": IndName,
         "_type": "DS",
-        # "_id": j,
         "_source": {
-            "scope":scope,
-            "fn":DS,
-            "creator":creator,
-            "type":datatype,
-            "files":filesSizes[0],
-            "size":filesSizes[1],
+            "scope":DS.scope,
+            "fn":DS.name,
+            "creator":DS.creator,
+            "type":DS.datatype,
+            "files":DS.files,
+            "size":DS.size,
             "timestamp": datetime.now()
             }
         }
     actions.append(action)
+
 
 helpers.bulk(es, actions)
