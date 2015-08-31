051b9f70e51001725cca92c2ea482f4f09c783c6
Ilija Vukotic <ivukotic@cern.ch>
2015-08-31 13:47:26 -0500

    scr

--- Indexer.py
+++ Indexer.py
@@ -7,4 +7,21 @@ es.indices.delete(index='LocalGroupDiskDatasets', ignore=[400, 404])
 es.indices.create(index='LocalGroupDiskDatasets', ignore=400)
 
 # add a doc
-es.indices.index(index='LocalGroupDiskDatasets',doc_type="ds",body="user.yang43:user.yang43.data11_7TeV.DAOD_ONIAMUMU.Period_M2M4M5M6M8M10.pro10_v01.Onia.17.0.4.1.v1.4.131226095813")
+es.indices.index(index='LocalGroupDiskDatasets',doc_type="ds",body="")
+
+actions = []
+while (j <= 10):
+    action = {
+        "_index": "LocalGroupDiskDatasets",
+        "_type": "DS",
+        "_id": j,
+        "_source": {
+            "scope":"user.yang43",
+            "fn":"user.yang43.data11_7TeV.DAOD_ONIAMUMU.Period_M2M4M5M6M8M10.pro10_v01.Onia.17.0.4.1.v1.4.131226095813" + str(j),
+            "timestamp": datetime.now()
+            }
+        }
+    actions.append(action)
+    j += 1
+
+helpers.bulk(es, actions)
