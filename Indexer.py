624af5430a2eb1cedd814f8b1ee9c37ee1f9d81c
Ilija Vukotic <ivukotic@cern.ch>
2015-08-31 13:52:00 -0500

    scr

--- Indexer.py
+++ Indexer.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 es = Elasticsearch("uct2-es-head:9200")
 
-es.indices.delete(index='LocalGroupDiskDatasets', ignore=[400, 404])
+es.indices.delete(index='local_group_disk_datasets', ignore=[400, 404])
 
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
-es.indices.create(index='LocalGroupDiskDatasets', ignore=400)
+es.indices.create(index='local_group_disk_datasets', ignore=400)
 
 actions = []
 j=0
 while (j <= 10):
     action = {
-        "_index": "LocalGroupDiskDatasets",
+        "_index": "local_group_disk_datasets",
         "_type": "DS",
         "_id": j,
         "_source": {
